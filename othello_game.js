c74997911a38f98770ab078e12daed116aa48950
fchavonet <fchavonet@mailo.com>
2024-09-21 18:57:32 +0200

    Creating the function that draws the pieces.

--- othello_game.js
+++ othello_game.js
@@ -1,11 +1,23 @@
 // References to game container and layer elements.
 const othelloGameContainer = document.getElementById("othello_game_container");
 const gameBoardLayer = document.getElementById("game_board_layer");
+const piecesLayer = document.getElementById("pieces_layer");
 
 // Global variables.
 const cellSize = 65;
 const cellBorderRadius = 2.5;
 const gap = 3;
+
+var pieces = [
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 2, 1, 0, 0, 0],
+	[0, 0, 0, 1, 2, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 // Set container size and initialize board on window load.
 window.onload = function () {
@@ -13,7 +25,8 @@ window.onload = function () {
 	othelloGameContainer.style.minHeight = (cellSize * 8) + (gap * 9) + "px";
 
 	drawGameBoard();
-	addCornerMarkers();
+	drawCornerMarkers();
+	drawPieces();
 }
 
 // Create and position each cell of the game board.
@@ -35,7 +48,7 @@ function drawGameBoard() {
 }
 
 // Add corner markers to the the game board.
-function addCornerMarkers() {
+function drawCornerMarkers() {
 	const cornerMarkerPositions = [
 		{ top: 2, left: 2 },
 		{ top: 2, left: 6 },
@@ -55,4 +68,39 @@ function addCornerMarkers() {
 
 		gameBoardLayer.appendChild(cornerMarker);
 	});
+}
+
+//
+function drawPieces() {
+	piecesLayer.innerHTML = "";
+
+	for (let row = 0; row < 8; row++) {
+		for (let column = 0; column < 8; column++) {
+			//
+			let pieceState = pieces[row][column];
+
+			if (pieceState == 0) {
+				continue;
+			}
+			else {
+				const piece = document.createElement("div");
+				piece.style.position = "absolute";
+				piece.style.top = ((cellSize + gap) * row) + gap + 5 + "px";
+				piece.style.left = ((cellSize + gap) * column) + gap + 5 + "px";
+				piece.style.width = (cellSize - 10) + "px";
+				piece.style.height = (cellSize - 10) + "px";
+				piece.style.borderRadius = "50%"
+				piece.style.boxShadow = "0 2.5px 5px rgba(0, 0, 0, 0.5)"
+
+				if (pieceState == 1) {
+					piece.style.backgroundColor = "var(--black)"
+				}
+				else if (pieceState == 2) {
+					piece.style.backgroundColor = "var(--white)"
+				}
+
+				piecesLayer.appendChild(piece);
+			}
+		}
+	}
 }
